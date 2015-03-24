5c355d00cac8c15b8572d75aa9137984200bc048
genericallyTerrible <jmoehrnkel@gmail.com>
2015-03-24 11:00:01 -0400

    Renamed project to reflect what it actually is.

--- src/ElevatorProj_TestPackage/Elevator.java
+++ src/ElevatorProj_TestPackage/Elevator.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 
-package STEMproj_TestPackage;
+package ElevatorProj_TestPackage;
 
 /**
  *
--- src/ElevatorProj_TestPackage/ElevatorSystem.java
+++ src/ElevatorProj_TestPackage/ElevatorSystem.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 
-package STEMproj_TestPackage;
+package ElevatorProj_TestPackage;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
